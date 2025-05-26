7ffd60b1cdf9d0495c9bb4433ab5d490ecc93ebc
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2025-05-26 22:27:08 +0800

    update title

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -3,8 +3,8 @@ import type { UserConfig } from '~/types'
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   site: {
-    title: '韩宇杰',
-    subtitle: '流年印记',
+    title: '流年印记',
+    subtitle: '韩宇杰的个人博客',
     author: '韩宇杰',
     description: '记录个人的成长与探索',
     website: 'https://www.hanyujie.xyz/',
